b981aa6026fda5ced42b1e27b974653e05428279
za3bor <fadisrour.za3bor@gmail.com>
2024-03-03 17:08:46 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+var app = angular.module('imageApp', []);
+
+app.controller('ImageController', function($scope, $interval) {
+  $scope.images = [
+    { thumbnail: 'https://picsum.photos/id/71/50/50', fullsize: 'https://picsum.photos/id/71/400/400' },
+    { thumbnail: 'https://picsum.photos/id/102/50/50', fullsize: 'https://picsum.photos/id/102/400/400' },
+    { thumbnail: 'https://picsum.photos/id/13/50/50', fullsize: 'https://picsum.photos/id/13/400/400' },
+    { thumbnail: 'https://picsum.photos/id/185/50/50', fullsize: 'https://picsum.photos/id/185/400/400' },
+    { thumbnail: 'https://picsum.photos/id/20/50/50', fullsize: 'https://picsum.photos/id/20/400/400' },
+    { thumbnail: 'https://picsum.photos/id/100/50/50', fullsize: 'https://picsum.photos/id/100/400/400' },
+  ];
+
+  $scope.currentIndex = null;
+
+  $scope.showFullSize = function(index) {
+    $scope.currentIndex = index;
+  };
+
+  $scope.prev = function() {
+    $scope.currentIndex = ($scope.currentIndex - 1 + $scope.images.length) % $scope.images.length;
+  };
+
+  $scope.next = function() {
+    $scope.currentIndex = ($scope.currentIndex + 1) % $scope.images.length;
+  };
+
+  var intervalPromise = $interval(function() {
+    $scope.currentIndex = ($scope.currentIndex + 1) % $scope.images.length;
+  }, 2000);
+
+  $scope.$on('$destroy', function() {
+    $interval.cancel(intervalPromise);
+  });
+});
